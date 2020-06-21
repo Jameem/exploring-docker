3f37616150cfa08a8c4a9ea99beff95b389bb182
Jameem <jameemkurikkal.mp@gmail.com>
2020-06-21 08:11:26 +0530

    integrating redis

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express")
+const redis = require("redis")
 
 const app = express()
+const client = redis.createClient()
+client.set("visits", 0)
 
 app.get("/", (req, res) => {
-  res.send("Hi there")
+  client.get("visists", (err, visits) => {
+    res.send("Number of Visits: " + visits)
+  })
+
+  client.set("visits", +visits + 1)
 })
 
-app.listen(8080, () => console.log("Listening on port 8080"))
+app.listen(8081, () => console.log("Listening on port 8081"))
