1162bb412f9434447beaf804769f3ac2ec6fcb18
Jameem <jameemkurikkal.mp@gmail.com>
2020-06-21 10:13:52 +0530

    setting up visit count

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const client = redis.createClient({
 client.set("visits", 0)
 
 app.get("/", (req, res) => {
-  client.get("visists", (err, visits) => {
+  client.get("visits", (err, visits) => {
     res.send("Number of Visits: " + visits)
+    client.set("visits", +visits + 1)
   })
-
-  client.set("visits", +visits + 1)
 })
 
 app.listen(8081, () => console.log("Listening on port 8081"))
